aece55fa50270c9a5ac867eab5e6ed91a723dda3
Lucas <lrdrocha@inf.ufpel.edu.br>
2017-01-09 16:39:30 -0200

    Update Mips.vhd
    Primeira modificação no codigo da arquitetura mips, adicionando os mux e os sinais de controle do mux

--- MIPS.vhd
+++ MIPS.vhd
@@ -12,7 +12,7 @@ end MIPS;
 architecture rtl of MIPS is
 	signal sig_OUT_PCP4_2, sig_OUT_jump, sig_OUT_memI_1, sig_inst, sig_regData, sig_dadoLido1, sig_dadoLido2,
 	sig_imediate_ext, sig_dadoLido1_1,sig_dadoLido2_1, sig_imediate_ext_1, sig_somInPC,
-	sig_IN2_ULA, sig_ULA_result, sig_OUT_PCP4_3, sig_in_PC, sig_out_PC,
+	sig_IN1_ULA,sig_IN2_ULA, sig_ULA_result, sig_OUT_PCP4_3, sig_in_PC, sig_out_PC,
 	sig_OUT_jump_1, sig_ULA_result_1, sig_dadoLido2_2, sig_OUT_memD, 
 	sig_OUT_memD_1, sig_ULA_result_2, sig_OUT_PCP4_1: std_logic_vector(31 downto 0);
 	signal in_PIPE1, out_PIPE1: std_logic_vector(63 downto 0);
@@ -20,16 +20,18 @@ architecture rtl of MIPS is
 	signal sig_ReadReg1, sig_ReadReg2, sig_regDest, sig_RegEsc, sig_ReadReg2_1, sig_regDest_1,
 	sig_RegEsc_1, sig_RegEsc_0, sig_RegEsc_2 : std_logic_vector (4 downto 0);
 	signal sig_imediate: std_logic_vector(15 downto 0);
-	signal sig_ulaOp, sig_ulaOp_1: std_logic_vector(1 downto 0);
+	signal sig_adiantaB1,sig_adiantaA1 ,sig_adiantaA ,sig_adiantaB ,sig_ulaOp , sig_ulaOp_1: std_logic_vector(1 downto 0);
 	signal sig_ulaFonte, sig_ulaFonte_1, sig_escMem, sig_escMem_1, sig_lerMem, 
 	sig_lerMem_1, sig_DvC, sig_DvC_1, sig_memParaReg, sig_memParaReg_1, sig_fontePC, we3, 
 	sig_escReg_1, sig_ULA_zero, sig_RegDST, sig_escMem_2, sig_lerMem_2,
 	sig_DvC_2, sig_memParaReg_2, sig_escReg_2, sig_ULA_zero_1, sig_memParaReg_3, sig_escReg_3, sig_escReg, sig_RegDST_1 : STD_LOGIC;
-	signal in_PIPE2, out_pipe2: std_logic_vector( 146 downto 0);
 	signal sig_operULA: std_logic_vector(3 downto 0);
 	signal in_PIPE3,  out_PIPE3: std_logic_vector (106 downto 0);
 	signal in_PIPE4, out_PIPE4: std_logic_vector(70 downto 0);
-
+---------------------------------------------------------------------------------------
+	signal in_PIPE2, out_pipe2: std_logic_vector( 145 downto 0); -- MODIFICADO , RETIRADO BIT NO SIG_ULAFONTE
+	signal in_PIPEAUX,out_pipeaux:std_logic_vector( 3 downto 0); -- 4 bits
+---------------------------------------------------------------------------------------
 	component reg
 		generic(
 			DATA_WIDTH : natural := 8
@@ -98,6 +100,18 @@ architecture rtl of MIPS is
 		);
 	end component;
 	
+	component mux4to1 
+		generic(
+			DATA_WIDTH : natural := 32
+		);
+		Port (
+			SEL : in  STD_LOGIC_VECTOR(1 DOWNTO 0);
+			A, B, C, D : in  STD_LOGIC_VECTOR((DATA_WIDTH-1) downto 0);
+			X   : out STD_LOGIC_VECTOR((DATA_WIDTH-1) downto 0)
+		);
+   end component;
+	
+	
 	component PC
 		generic(
 			DATA_WIDTH : natural := 5
@@ -124,13 +138,14 @@ architecture rtl of MIPS is
 			out32: out std_logic_vector(31 downto 0)
 		);
 	end component;
-	
+	---Ula fonte retirado , adicionado adiantaA E adiantaB
 	component controller
 		PORT (
-			opcode : IN std_logic_vector(5 downto 0);
-			ulaOp : out std_logic_vector(1 downto 0);
-			RegDst, ulaFonte, escMem, lerMem, DvC, memParaReg, escReg : out std_logic
-		);
+		opcode : IN std_logic_vector(5 downto 0);
+		ulaOp : out std_logic_vector(1 downto 0);
+		RegDst, escMem, lerMem, DvC, memParaReg, escReg: out std_logic
+	   adiantaA,adiantaB : out std_logic_vector(1 downto 0); 
+	);
 	END component;
 	
 	component flipflop
@@ -207,22 +222,30 @@ begin
 	sig_OUT_PCP4_2 <= out_PIPE1(63 downto 32);
 	sig_inst <= out_PIPE1(31 downto 0);
 	
-	sig_opcode <= sig_inst(31 downto 26);
+	sig_opcode   <= sig_inst(31 downto 26);
 	sig_ReadReg1 <= sig_inst(25 downto 21);
 	sig_ReadReg2 <= sig_inst(20 downto 16);
 	sig_imediate <= sig_inst(15 downto 0);
-	sig_regDest <= sig_inst(15 downto 11);
+	sig_regDest  <= sig_inst(15 downto 11);
 
+	
+	--Nao funciona por que nao tem adiantaA adiantaB
 	controle: controller PORT MAP (
 		opcode => sig_opcode,
 		ulaOp => sig_ulaOp,
-		RegDST => sig_RegDST,
-		ulaFonte => sig_ulaFonte,
+		RegDST => sig_RegDST, 
+		---------------------------------------------------------------------------
+		-- NOVO SINAL DE DOIS BITS TAMBEM QUE CONTROLARA O OUTRO MULTIPLEXADOR
+		---------------------------------------------------------------------------
+		--ulaFonte => sig_ulaFonte, -- deve ser modificado  de dois bits para  4 bits
+		---------------------------------------------------------------------------
 		escMem => sig_escMem,
 		lerMem => sig_lerMem,
 		DvC => sig_DvC,
 		memParaReg => sig_memParaReg,
-		escReg => sig_escReg
+		escReg => sig_escReg,
+		adiantaA =>sig_adiantaA,
+		adiantaB =>sig_adiantaB
 	);
     
 	registradores: regbank PORT MAP (
@@ -241,21 +264,34 @@ begin
 		in16 => sig_imediate,
 		out32 => sig_imediate_ext
 	);
-
-	in_PIPE2 <= sig_ulaOp & sig_RegDST & sig_ulaFonte & sig_escMem & sig_lerMem & sig_DvC & sig_memParaReg & sig_escReg & sig_OUT_PCP4_2 & sig_dadoLido1 & sig_dadoLido2 & sig_imediate_ext & sig_ReadReg2 & sig_regDest;
-
-	PIPE2: flipflop GENERIC MAP (DATA_WIDTH => 147) PORT MAP (
+												--sig_ulaFonte	RETIRADO
+	in_PIPE2 <= sig_ulaOp & sig_RegDST & sig_escMem & sig_lerMem & sig_DvC & sig_memParaReg & sig_escReg & sig_OUT_PCP4_2 & sig_dadoLido1 & sig_dadoLido2 & sig_imediate_ext & sig_ReadReg2 & sig_regDest;
+	
+	-----------------------------------------------------------------------------
+	in_PIPEAUX <= sig_adiantaA & sig_adiantaB;
+	-----------------------------------------------------------------------------
+	
+	PIPE2: flipflop GENERIC MAP (DATA_WIDTH => 146) PORT MAP (
 		clk => clk,
 		rst => rst,
 		D => in_PIPE2,
 		Q => out_PIPE2
 	);
-
+--------------------------------------------------------------------------------
+	PIPEAUX: flipflop GENERIC MAP(DATA_WIDTH => 4) PORT MAP(
+		clk => clk,
+		rst => rst,
+		D	 => in_PIPEAUX,
+		Q	 => out_pipeaux
+		);
+--------------------------------------------------------------------------------
 	-- TERCEIRO ESTÁGIO --
 
-	sig_ulaOp_1 <= out_PIPE2(146 downto 145);
-	sig_RegDST_1 <= out_PIPE2(144);
-	sig_ulaFonte_1 <= out_PIPE2(143);
+	sig_ulaOp_1 <= out_PIPE2(145 downto 144);
+	sig_RegDST_1 <= out_PIPE2(143);
+	-------------------------------------------------------------------------------
+--	sig_ulaFonte_1 <= out_PIPE2(143); -- modificar aqui para receber outro sinal
+	-------------------------------------------------------------------------------
 	sig_escMem_1 <= out_PIPE2(142);
 	sig_lerMem_1 <= out_PIPE2(141);
 	sig_DvC_1 <= out_PIPE2(140);
@@ -268,9 +304,14 @@ begin
 	sig_function <= sig_imediate_ext_1(5 downto 0);
 	sig_ReadReg2_1 <= out_PIPE2(9 downto 5);
 	sig_regDest_1 <= out_PIPE2(4 downto 0);
-
 	sig_somInPC <= sig_imediate_ext_1(29 downto 0) & "00";
-
+------------------------------------------------------------------
+		--RECEBENDO OS SINAIS DO PIPE AUX E MANDANDO PROS CONTROLADORE DOS MULTIPLEXADORES
+	sig_adiantaA1 <= out_pipeaux(3 DOWNTO 2);
+	sig_adiantaB1 <= out_pipeaux(1 DOWNTO 0);
+------------------------------------------------------------------
+	
+	
 	inPC: addSub GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
 		a => sig_OUT_PCP4_3,
 		b => sig_somInPC,       --b de 10 recebe de 32 --  
@@ -278,13 +319,26 @@ begin
 		result => sig_OUT_jump
 	);
 
-	mux_IN_ULA_2: mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
-		sel => sig_ulaFonte_1,
-		A => sig_dadoLido2_1,
-		B => sig_imediate_ext_1,
-		X => sig_IN2_ULA
+------------------------------------------------------------------------------------	
+	
+	mux_IN_ULA_4_0: mux4to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+		sel => sig_adiantaA1 ,
+		A => sig_dadoLido1_1 , -- PRIMEIRA SAIDA DO BANCO DE REGISTRADORES
+		B => sig_ULA_result_1, -- SEGUNDA EH A REDIRECAO DA SAIDA DA ULA POS FLIP FLOP
+		C => sig_regData , -- TERCEIRA EH A REDIRECAO DA SAIDA DEPOIS DO MULTIPLEXADOR
+		D =>"00000000000000000000000000000000",
+		X => sig_IN1_ULA -- PRIMEIRA ENTRADA DE ULA
 	);
-
+------------------------------------------------------------------------------------	
+	mux_IN_ULA_4_0: mux4to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+		sel => sig_adiantaB1  ,
+		A => sig_dadoLido2_1  ,
+		B => sig_ULA_result_1 , -- SEGUNDA EH A REDIRECAO DA SAIDA DA ULA POS FLIP FLOP
+		C => sig_regData , -- TERCEIRA EH A REDIRECAO DA SAIDA DEPOIS DO MULTIPLEXADOR
+		D => sig_somInPC , -- IMEDIATO POS FLIP FLOP
+		X => sig_IN1_ULA -- SEGUNDA ENTRADA DE ULA
+		);
+------------------------------------------------------------------------------------
 	operaULA: opULA PORT MAP (
 		ULAop => sig_ulaOp_1,
 		funct => sig_function,
@@ -292,7 +346,7 @@ begin
 	);
 
 	ULA1: ULA PORT MAP (
-		in0 => sig_dadoLido1_1,
+		in0 => sig_IN1_ULA --sig_dadoLido1_1, -- modificado para outro sinal , guardar sinal de saido do banco de register
 		in1 => sig_IN2_ULA,
 		oper => sig_operULA,
 		zero => sig_ULA_zero,
@@ -324,7 +378,9 @@ begin
 	sig_escReg_2 <= out_PIPE3(102);
 	sig_OUT_jump_1 <= out_PIPE3(101 downto 70);
 	sig_ULA_zero_1 <= out_PIPE3(69);
-	sig_ULA_result_1 <= out_PIPE3(68 downto 37);
+	-----------------------------------------------------------------------
+	sig_ULA_result_1 <= out_PIPE3(68 downto 37); -- redirecionar
+	-----------------------------------------------------------------------
 	sig_dadoLido2_2 <= out_PIPE3(36 downto 5);
 	sig_RegEsc_1 <= out_PIPE3(4 downto 0);
 
@@ -363,5 +419,6 @@ begin
 	);
 	
 	outs <= sig_regData;
-
+	--sig_regData redirecionar
+	
 end rtl;
