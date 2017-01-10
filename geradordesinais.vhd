81b17bcd0a443c1138c9a16e628d05c64a5fb7ff
Lucas <lrdrocha@inf.ufpel.edu.br>
2017-01-10 19:56:10 -0200

    Update gerador de sinais
    foi adicionado novo conteudo nos multiplexadores.

--- geradordesinais.vhd
+++ geradordesinais.vhd
@@ -23,36 +23,88 @@ begin
 	begin
 		if(opcode="000000") then
 			if(sinal1='1' and sinal2 ='1')then -- reg2 = rs0 
-					AdiantaA="10";
-					AdiantaB="10";
+					AdiantaA <= "10";
+					AdiantaB <= "10";
 			if(sinal3='1' and sinal4 ='1')then
-					AdiantaA="01";
-					AdiantaB="01";
+					AdiantaA <= "01";
+					AdiantaB <= "01";
 			if(sinal1='1' and sinal4='1')then
-					AdiantaA = "10";
-					AdiantaB = "01";
+					AdiantaA <= "10";
+					AdiantaB <= "01";
 			if(sinal2 ='1' and sinal3 = '1')then
-					AdiantaA ="01";
-					AdiantaB ="10";
+					AdiantaA <="01";
+					AdiantaB <="10";
 			if(sinal1='1' and sinal2='0' and sinal3='0' and sinal4='0')then
-					AdiantaA = "10";
-					AdiantaB = "00";
+					AdiantaA <= "10";
+					AdiantaB <= "00";
 			if(sinal1='0' and sinal2='1' and sinal3='0' and sinal4='0')then
-					AdiantaA = "00";
-					AdiantaB = "10";
+					AdiantaA <= "00";
+					AdiantaB <= "10";
 			if(sinal1='0' and sinal2='0' and sinal3='1' and sinal4='0')then
-					AdiantaA = "01";
-					AdiantaB = "00";
+					AdiantaA <= "01";
+					AdiantaB <= "00";
 			if(sinal1='0' and sinal2='0' and sinal3='0' and sinal4='1')then
-					AdiantaA = "00";
-					AdiantaB = "01";
+					AdiantaA <= "00";
+					AdiantaB <= "01";
 			end if;
 		if(opcode="100011")then --lw
+			if(sinal1='0' and sinal3='0')then
+				AdiantaA <= "00";
+				AdiantaB <= "11";
+			if(sinal1='1')then
+				AdiantaA <= "10";
+				AdiantaB <= "11";
+			if(sinal3='1')then
+				AdiantaA<="01";
+				AdiantaB<="11";
+			end if;
 		if(opcode="101011")then --sw
+			if(sinal1='0' and sinal3='0')then
+				AdiantaA <= "00";
+				AdiantaB <= "11";
+			if(sinal1='1')then
+				AdiantaA <= "10";
+				AdiantaB <= "11";
+			if(sinal3='1')then
+				AdiantaA<="01";
+				AdiantaB<="11";
+			end if;
 		if(opcode="000100")then --beq
+			if(sinal1='0' and sinal3='0')then
+				AdiantaA <= "00";
+				AdiantaB <= "11";
+			if(sinal1='1')then
+				AdiantaA <= "10";
+				AdiantaB <= "11";
+			if(sinal3='1')then
+				AdiantaA<="01";
+				AdiantaB<="11";
+			end if;
 		if(opcode="001000")then --addi
+			if(sinal1='0' and sinal3='0')then
+				AdiantaA <= "00";
+				AdiantaB <= "11";
+			if(sinal1='1')then
+				AdiantaA <= "10";
+				AdiantaB <= "11";
+			if(sinal3='1')then
+				AdiantaA<="01";
+				AdiantaB<="11";
+			end if;
 		if(opcode="001101")then --ori
+			if(sinal1='0' and sinal3='0')then
+				AdiantaA <= "00";
+				AdiantaB <= "11";
+			if(sinal1='1')then
+				AdiantaA <= "10";
+				AdiantaB <= "11";
+			if(sinal3='1')then
+				AdiantaA<="01";
+				AdiantaB<="11";
+			end if;
 		if(opcode="000010")then --jumps
+			AdiantaA<="00";
+			AdiantaB<="11";
 		else
 		
 		end if;
