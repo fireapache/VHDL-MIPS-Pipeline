92c873d5a4d399ee5fa024e9ee3231598f541144
LucasRR94 <lucasregodarocha@outlook.com>
2017-02-27 14:24:08 -0300

    novas variaveis declaradas

--- MIPS.vhd
+++ MIPS.vhd
@@ -1,7 +1,7 @@
 library ieee;
 use ieee.numeric_std.all;
 use IEEE.STD_LOGIC_1164.ALL;
-
+--issue descobrir se eh fontepc_ ou controler_mux_firstmux
 entity MIPS is
     Port (
 		outs: out std_logic_vector(31 downto 0);
@@ -35,9 +35,21 @@ architecture rtl of MIPS is
 	signal sig_saida_bit_sujo1,sig_saida_bit_sujo2,sig_saida_bit_sujo3,saida_bit_sujo1,saida_bit_sujo2,saida_bit_sujo3 : std_logic; -- saidas dos pipes dos bitsujos
 	------------------------------------------------------------------------------------
 	signal saidaprimeiromux,saidasegundopipe,saidaprimeiropipe ,saidadoprimeiroreg ,saidaor: std_logic;
-	signal opcode_sig :std_logic_Vector(5 downto 0);
-	signal entrada0_mux,entrada1_mux :std_logic_Vector(1 downto 0);
-	signal saida_bit_sujo2_1:std_logic;
+	signal sig_imediate_extende2  : std_LOGIC_VECTOR(31 downto 0); -- extende o 2 , manda o sinal extendido 2 estagio.
+	signal sig_out_dinamic_control: std_LOGIC_VECTOR(31 downto 0); -- saida das decisoes jumps
+	signal dinamic_controle_1,dinamic_controle : std_logic;-- controla os dois mux do segundo estagio
+	signal sig_out_pulo2 : std_logic_vector(31 downto 0);
+	signal sig_out_dinamic_control_1 : std_LOGIC_VECTOR(31 downto 0);
+	signal sig_control_first_muxs :std_LOGIC; -- controla o problema se o beq pulou errado , la nos primeiros muxs 
+	signal sig_ehbeqadiantado,sig_ehbeqadiantado_ponte,sig_control_recovery_pc : std_logic; -- sinal que sai do controle e transmite se eh o beq adiantado
+	signal sig_in_PC_out  : std_logic_Vector(31 downto 0);
+	signal sig_out_recovery_1 , sig_out_recovery_2 , sig_out_recovery_3 :std_logic_vector(31 downto 0);
+	-- variaveis de troca entre os flip flops
+	signal sig_tratamento_presoma ,sig_tratamento_recovery :std_logic_Vector(31 downto 0);
+	-- VARIAVEIS QUE SAO PRE SOMA E POS SOMA
+	signal in_PIPE_ehbeqadiantado,in_PIPE_ehbeqadiantado2,sig_saida_ehbeqadiantado ,out_PIPE_ehbeq2,out_PIPE_ehbeq: std_logic; -- faz o sinal passar pelos pipe , em controlar o primeiro mux
+	signal ehbeqadiantado :std_logic;
+
 	component reg
 		generic(
 			DATA_WIDTH : natural := 8
@@ -48,6 +60,7 @@ architecture rtl of MIPS is
 			Q : out std_logic_vector ((DATA_WIDTH-1) downto 0)
 		); 
 	END component ;
+	
 	component mux2to11bit
 		Port (
 		SEL : in  STD_logic;
@@ -55,6 +68,7 @@ architecture rtl of MIPS is
 		X   : out STD_LOGIC
 	);
 	end component;
+	
 	component memInst
 		PORT(
 			address	: IN STD_LOGIC_VECTOR (9 DOWNTO 0);
@@ -150,14 +164,7 @@ architecture rtl of MIPS is
 			out32: out std_logic_vector(31 downto 0)
 		);
 	end component;
-	------------------------------------------------------------
-	component gerador_sinais_simples 
-		port(	
-		   opcode : in std_logic_Vector(5 downto 0);
-			AdiantaA1,AdiantaB1:out std_logic_vector(1 downto 0)
-		 );
-	end component;
-	------------------------------------------------------------
+	---Ula fonte retirado , adicionado adiantaA E adiantaB
 	component controller
 		PORT (
 		clk : in std_logic;
@@ -166,6 +173,8 @@ architecture rtl of MIPS is
 		ulaOp : out std_logic_vector(1 downto 0);
 		RegDst, escMem, lerMem, DvC, memParaReg, escReg: out std_logic;
 		reg1,reg2,reg3 : in std_logic_vector(4 downto 0);
+		fontepc			: in std_logic;
+		ehbeqadiantado : out std_logic;
 		adiantaA,adiantaB :out std_logic_vector(1 downto 0)
 	);
 	END component;
@@ -208,69 +217,57 @@ architecture rtl of MIPS is
 begin
 	
 	-- PRIMEIRO ESTÁGIO --
-	-----------------------------------------------------------------
-	sig_control_recovery_pc <= (sig_control_first_muxs  and sig_ehbeqadiantado);
-	-----------------------------------------------------------------
-	dinamic_controle_1 <= (ehbeqadiantado and sig_DvC);
-	-----------------------------------------------------------------
-	MUX_dinamic_controle_1 : mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
-		sel => dinamic_controle_1, 	  -- dinamic_controle 
-		A => sig_OUT_jump_1,		 	  -- jump_do_4 estagio , 
-		B => sig_out_pulo2,  -- o imediato extendido e com 2 bits a esquerda somado com +4 = pulo do beq imediato
-		X => sig_out_dinamic_control-- fio de saida entra o saida + 4 
+	
+	PC1: PC GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+		clk => clk,
+		rst => rst,
+		D => sig_in_PC_out,
+		Q => sig_out_PC
 	);
-	-----------------------------------------------------------------
+	---------------------------------------------------
+	-- (sig_control_first_muxs  and sig_ehbeqadiantado)-- controla os muxs do beq 
+	dinamic_controle_1 <= (ehbeqadiantado and sig_DvC);
 	dinamic_controle   <= ((sig_fontePC and not(ehbeqadiantado)) or (dinamic_controle_1 ));
-	-----------------------------------------------------------------
-	MUX_CONTROLA_JUMP_OR_PC: mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (  --1
+	----------------------------------------------------
+	mux_IN_PC: mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
 		sel => dinamic_controle,
-		A => sig_OUT_PCP4_1, --sai do pc+4
-		B => sig_out_dinamic_control, -- Vem do segundo estagio do circuitos
+		A => sig_OUT_PCP4_1,
+		B => sig_out_dinamic_control,
 		X => sig_in_PC
 	);
 	-----------------------------------------------------
-	sig_control_recovery_pc <= (sig_control_first_muxs  and sig_ehbeqadiantado);
-	-----------------------------------------------------------------
-	MUX_CONTROLA_PC : mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP ( --2 
-		sel => sig_control_recovery_pc , 	  
-		A =>  sig_in_PC, -- entra saida do multiplex, 		
-		B =>  sig_tratamento_recovery,  -- entra tratamento de beq mal sucedido
-		X =>  sig_in_PC_out -- entra do pc
-	);
-	------------------------------------------------------------------
-	
-	
-	PC1: PC GENERIC MAP (DATA_WIDTH => 32) PORT MAP ( 
-		clk => clk,
-		rst => rst,
-		D => sig_in_PC_out,--sig_in_PC,
-		Q => sig_out_PC
-	);
-	
-	------------------------------------------------------------------
 	PCP4: addSub GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
 		a => sig_out_PC,
 		b => "00000000000000000000000000000100", -- 4
 		add_sub => '1',
 		result => sig_OUT_PCP4_1
 	);
-	------------------------------------------------------------------
-	PRIMEIRO_PIPE_recovery: flipflop GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+	-----------------------------------------------------
+	sig_control_recovery_pc <= (sig_control_first_muxs  and sig_ehbeqadiantado);
+	
+	mux_recovery : mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+		sel => sig_control_recovery_pc , 	  
+		A =>  sig_in_PC, -- entra saida do multiplex, 		
+		B =>  sig_tratamento_recovery,  -- entra tratamento de beq mal sucedido
+		X =>  sig_in_PC_out -- saida entrada do pc
+	);
+	-------------------------------------------------------------------
+	PIPE_recovery: flipflop GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
 		clk => clk,
 		rst => rst,
 		D => sig_out_PC,
 		Q => sig_out_recovery_1
 	);
 	-------------------------------------------------------------------
-	SEGUNDO_PIPE_recovery: flipflop GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+	PIPE_1_recovery: flipflop GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
 		clk => clk,
 		rst => rst,
 		D => sig_out_recovery_1,
 		Q => sig_out_recovery_2
 	);
 	-------------------------------------------------------------------
-	MUX_DE_RECOVERY : mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP ( --3
-		sel => sig_control_first_muxs , 	   
+	mux_recovery_2 : mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+		sel => sig_control_first_muxs , 	  
 		A =>  "00000000000000000000000000000000", -- entra saida do multiplex, 		
 		B => sig_out_recovery_2,  -- entra tratamento de beq mal sucedido
 		X => sig_tratamento_presoma -- saida entrada do pc
@@ -282,8 +279,8 @@ begin
 		add_sub => '1',
 		result => sig_tratamento_recovery
 	);
-	-----------------------------------------------------------------
-	------------------------------------------------------------------
+	
+	-----------------------------------------------------
 	memI: memInst2 PORT MAP (
 		address	 => sig_out_PC(11 downto 2),
 		clock	 => clk,
@@ -312,7 +309,7 @@ begin
 	sig_imediate <= sig_inst(15 downto 0);	 -- imediato
 	sig_regDest  <= sig_inst(15 downto 11); -- registrador destino
 
-		
+	
 	controle: controller PORT MAP (
 		clk => clk,
 		rst => rst,
@@ -329,9 +326,11 @@ begin
 		DvC     => sig_DvC,
 		memParaReg => sig_memParaReg,
 		escReg  => sig_escReg,
-		reg1    => sig_regDest, -- registrador (15 downto 11)
+		reg1    => sig_regDest, -- registrador destino
 		reg2    => sig_ReadReg1,-- registrador dado lido 1
-		reg3    => sig_ReadReg2,-- registrador dado lido 2 (20 downto 16)
+		reg3    => sig_ReadReg2,-- registrador dado lido 2
+		fontepc => sig_fontePC2,
+		ehbeqadiantado=>sig_ehbeqadiantado_ponte,-- faz and con dvc not(zero_ula)
 		adiantaA => sig_adiantaA,
 		adiantaB => sig_adiantaB
 	);
@@ -352,13 +351,50 @@ begin
 		in16 => sig_imediate,
 		out32 => sig_imediate_ext
 	);
-												--sig_ulaFonte	RETIRADO
+	ehbeqadiantado <= sig_ehbeqadiantado_ponte;
+	--sig_imediate_ext_1<=sig_imediate_ext;	--adiciona a referencia ao extensor
 	in_PIPE2 <= sig_ulaOp & sig_RegDST & sig_escMem & sig_lerMem & sig_DvC & sig_memParaReg & sig_escReg & sig_OUT_PCP4_2 & sig_dadoLido1 & sig_dadoLido2 & sig_imediate_ext & sig_ReadReg2 & sig_regDest;
+	in_PIPE_ehbeqadiantado <= sig_ehbeqadiantado_ponte;
+	-----------------------------------------------------------------------------
+	-- extender dois bits a esquerda a partir do fio , --sig_imediate_ext
+	-- declarado o sinal sig_imediate_extende2
+	sig_imediate_extende2 <= sig_imediate_ext(29 downto 0) & "00"; -- foi por extendido pra 32 bits o beq
+	-----------------------------------------------------------------------------
+	-- pegar endereço atual e somar com o 32 bits de beq
+	som_pulo_imediate: addSub GENERIC MAP (DATA_WIDTH => 32) PORT MAP ( -- soma endereco atual com o pulo do beq
+		a => sig_imediate_extende2,
+		b => sig_OUT_PCP4_2, -- 4
+		add_sub => '1',
+		result => sig_out_pulo2
+	);
+	-- fio sig_out_pulo2 entra no mux na entrada 1 , mux controlado por dinamic_controle
+	-- entrada 0 :sig_OUT_jump_1
+	-----------------------------------------------------------------------------
+	-- fio de saida mux antes do controle de jump:sig_out_dinamic_control
+	
+	mux_dinamic_controle_1 : mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+		sel => dinamic_controle_1, 	  -- dinamic_controle 
+		A => sig_OUT_jump_1,		 	  -- jump_do_4 estagio , 
+		B => sig_out_pulo2,  -- o imediato extendido e com 2 bits a esquerda somado com +4 = pulo do beq imediato
+		X => sig_out_dinamic_control-- fio de saida entra o saida + 4 
+	);
+	
+--	mux_dinamic_controle_2 : mux2to1 GENERIC MAP (DATA_WIDTH => 32) PORT MAP (
+--		sel => dinamic_controle, 	  -- dinamic_controle 
+--		A =>  sig_out_dinamic_control_1,		 	  -- saido do mux_controle_1 que tem como op jump do antes e o jump do 4 estagio
+--		B => sig_OUT_PCP4_2,  -- pc+4
+--		X => sig_out_dinamic_control
+--	);
 	
 	-----------------------------------------------------------------------------
 	in_PIPEAUX <= sig_adiantaA & sig_adiantaB;
 	-----------------------------------------------------------------------------
-	
+	PIPE_EH_beq_adiantado: flipflop1b PORT MAP(
+		clk =>clk,
+		rst =>rst,
+		D =>in_PIPE_ehbeqadiantado,
+		Q=>out_PIPE_ehbeq
+	);
 	PIPE2: flipflop GENERIC MAP (DATA_WIDTH => 146) PORT MAP (
 		clk => clk,
 		rst => rst,
@@ -381,12 +417,6 @@ begin
 		Q	 => out_pipeaux
 		);
 --------------------------------------------------------------------------------
-	PIPEOPCODE_EXTEND_ESTAGE : flipflop GENERIC MAP(DATA_WIDTH => 6) PORT MAP(
-		clk=>clk,
-		rst=>rst,
-		D=>sig_opcode,
-		Q=>opcode_sig
-	);
 	-- TERCEIRO ESTÁGIO --
 
 	sig_ulaOp_1 <= out_PIPE2(145 downto 144);
@@ -404,28 +434,12 @@ begin
 	sig_ReadReg2_1 <= out_PIPE2(9 downto 5);
 	sig_regDest_1 <= out_PIPE2(4 downto 0);
 	sig_somInPC <= sig_imediate_ext_1(29 downto 0) & "00";
+	sig_saida_ehbeqadiantado <=out_PIPE_ehbeq ;
+	
 ------------------------------------------------------------------
 		--RECEBENDO OS SINAIS DO PIPE AUX E MANDANDO PROS CONTROLADORE DOS MULTIPLEXADORES
-	gerarar_simples : gerador_sinais_simples port map(opcode_sig,entrada0_mux,entrada1_mux);
-	
-	muxcontroler_adiantaA: mux2to1 GENERIC MAP (DATA_WIDTH => 2) PORT MAP (
-		sel => saida_bit_sujo2_1, -- modificado
-		A => out_pipeaux(3 DOWNTO 2),
-		B => entrada0_mux,
-		X => sig_adiantaA1
-	);
-	muxcontroler_adiantaB: mux2to1 GENERIC MAP (DATA_WIDTH => 2) PORT MAP (
-		sel => saida_bit_sujo2_1, -- modificado
-		A => out_pipeaux(1 DOWNTO 0),
-		B => entrada1_mux,
-		X => sig_adiantaB1
-	);
-	
-	
-	
-	
---	sig_adiantaA1 <= out_pipeaux(3 DOWNTO 2);
---	sig_adiantaB1 <= out_pipeaux(1 DOWNTO 0);
+	sig_adiantaA1 <= out_pipeaux(3 DOWNTO 2);
+	sig_adiantaB1 <= out_pipeaux(1 DOWNTO 0);
 ------------------------------------------------------------------
 --RECEBE O SINAL DA SAIDA DO REGISTRADOR DE BIT SUJO
 	sig_saida_bit_sujo1 <= saida_bit_sujo1;
@@ -468,10 +482,9 @@ begin
 		in0 => sig_IN1_ULA, --sig_dadoLido1_1, -- modificado para outro sinal , guardar sinal de saido do banco de register
 		in1 => sig_IN2_ULA,
 		oper => sig_operULA,
-		zero => sig_ULA_zero,
+		zero => sig_ULA_zero, -- 
 		output => sig_ULA_result
 	);
-
 	muxEscReg: mux2to1 GENERIC MAP (DATA_WIDTH => 5) PORT MAP (
 		sel => sig_RegDST_1,
 		A => sig_ReadReg2_1,
@@ -483,18 +496,12 @@ begin
 		sel => sig_fontePC2,
 		A => sig_saida_bit_sujo1,
 		B => sig_fontePC2,
-		X => saida_bit_sujo2_1
+		X => saida_bit_sujo2
 	);
+
 ---------------------------------------------------------------------------------------------------
- PIPE_AUX_BIT_SUJO2: flipflop1b PORT MAP( -- pipe recem adicionado_teste
-		clk => clk,
-		rst => rst,
-		D   => saida_bit_sujo2_1,
-		Q   => saida_bit_sujo2	
-	);
-	
----------------------------------------------------------------------------------------------------
----------------------------------------------------------------------------------------------------
+
+   in_PIPE_ehbeqadiantado2 <= sig_saida_ehbeqadiantado;
 	in_PIPE3 <= sig_escMem_1 & sig_lerMem_1 & sig_DvC_1 & sig_memParaReg_1 & sig_escReg_1 & sig_OUT_jump & sig_ULA_zero & sig_ULA_result & sig_dadoLido2_1 & sig_RegEsc_0;
 
 	PIPE3: flipflop GENERIC MAP (DATA_WIDTH => 107) PORT MAP (
@@ -504,6 +511,13 @@ begin
 		Q => out_PIPE3
 	);
 
+	PIPE_EHBEQ_adiantado2: flipflop1b PORT map(
+		clk =>clk,
+		rst =>rst,
+		D  =>in_PIPE_ehbeqadiantado2,
+		Q  =>out_PIPE_ehbeq2
+	);
+	
 	-- QUARTO ESTÁGIO --
 	------------------------------------------------------------------------
 	sig_saida_bit_sujo2 <= saida_bit_sujo2; -- saida do pipe bit sujo 2
@@ -522,7 +536,9 @@ begin
 	sig_RegEsc_1 <= out_PIPE3(4 downto 0);
 	---------------------------------------------------------------
 	sig_fontePC <= sig_DvC_2 and sig_ULA_zero_1;
+	sig_control_first_muxs <= (sig_DvC_2 and (not (sig_ULA_zero_1)));
 	---------------------------------------------------------------
+	sig_ehbeqadiantado <= out_PIPE_ehbeq2;
 	memD: memData PORT MAP (
 		address	 => sig_ULA_result_1(11 downto 2),
 		clock	 => clk,
@@ -569,7 +585,7 @@ begin
 -----------------------------------------------------------------------------	
 	saidaor <= (saidaprimeiropipe or saidasegundopipe);
 -----------------------------------------------------------------------------	
-	PIPE_3_bit_sujo: flipflop1b PORT MAP (
+	PIPE_4_bit_sujo: flipflop1b PORT MAP (
 			clk => clk,
 			rst => rst,
 			D => sig_saida_bit_sujo2,
